4d50cdcd70e75381b53c9114015acd35cabfd8de
Ulises Vargas <uli.vargas02@gmail.com>
2024-03-02 16:55:46 -0600

    chore: cambios

--- index.ts
+++ index.ts
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 
 import { program } from 'commander'
-import updateNotifier from 'update-notifier'
 import { config, create } from './actions'
 import pkg from './package.json'
-
-const notifier = updateNotifier({pkg});
-notifier.notify();
 
 program
   .name("create-appyc")
